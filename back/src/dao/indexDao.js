839867f419e3706772219a863c6d9fbc871223d8
unknown <chn0202@gmail.com>
2022-07-21 20:31:45 +0900

    로그인 회원가입추가

--- back/src/dao/indexDao.js
+++ back/src/dao/indexDao.js
@@ -1,4 +1,23 @@
 const { pool } = require("../../config/database");
+// 로그인 (회원검증)
+exports.isValidUsers = async function (connection, userID, password) {
+  const Query = `SELECT userIdx, nickname FROM Users where userID = ? and password = ? and status = 'A';`;
+  const Params = [userID, password];
+
+  const rows = await connection.query(Query, Params);
+
+  return rows;
+};
+
+// 회원가입
+exports.insertUsers = async function (connection, userID, password, nickname) {
+  const Query = `insert into Users(userID, password, nickname) values (?,?,?);`;
+  const Params = [userID, password, nickname];
+
+  const rows = await connection.query(Query, Params);
+
+  return rows;
+};
 
 // 식당생성
 exports.selectRestaurants = async function (connection, category) {
--- back/src/routes/indexRoute.js
+++ back/src/routes/indexRoute.js
@@ -6,4 +6,10 @@ module.exports = function (app) {
   // app.HTTP메서드(uri, 컨트롤러 콜백함수)
 
   app.get("/restaurants", index.readRestaurants);
+  // 회원가입
+  app.post("/sign-up", index.createUsers);
+  // 로그인
+  app.post("/sign-in", index.createJwt);
+  // 로그인 유지, 토큰 검증
+  app.get("/jwt", jwtMiddleware, index.readJwt);
 };
